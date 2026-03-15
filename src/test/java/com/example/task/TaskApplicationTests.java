86cc2eebff8d3db0f71831d404d55e55e90766fa
Rostja <rasto.dorcak@gmail.com>
2026-03-15 22:12:56 +0100

    Adding basic JUnit tests

--- src/test/java/com/example/task/TaskApplicationTests.java
+++ src/test/java/com/example/task/TaskApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.task;
 
+import com.example.task.service.SearchService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class TaskApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testSearchResults() throws Exception {
+		SearchService service = new SearchService();
+
+		var results = service.search("kniha");
+
+		assertNotNull(results);
+		assertTrue(results.size() > 0);
 	}
 
 }
